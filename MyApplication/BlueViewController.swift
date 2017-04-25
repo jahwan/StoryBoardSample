a0c1f2f4f719966841e910f5dc58dea39e4dcf34
sds-001 <sds-001@70-12-240-184.pools.spcsdns.net>
2017-04-25 15:41:31 +0900

    화면추가 완료

---
+++ MyApplication/BlueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlueViewController.swift
+//  MyApplication
+//
+//  Created by SDS-001 on 2017. 4. 25..
+//  Copyright © 2017년 SDS-001. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
