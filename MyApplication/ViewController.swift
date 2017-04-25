0edb58bed181327a4103ee03c4fde677f39cf662
sds-001 <sds-001@70-12-240-184.pools.spcsdns.net>
2017-04-25 15:21:06 +0900

    Initial Commit

---
+++ MyApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyApplication
+//
+//  Created by SDS-001 on 2017. 4. 25..
+//  Copyright © 2017년 SDS-001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
